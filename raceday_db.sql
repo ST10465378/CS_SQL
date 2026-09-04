a8bf5f50e323ad3e7d18cfac2e0070b7f4465cee
ST10465378 <ST10465378@rcconnect.edu.za>
2026-09-04 15:01:58 +0200

    Create Table Users

--- raceday_db.sql
+++ raceday_db.sql
@@ -1,3 +1,18 @@
 --create raceday database
 CREATE DATABASE RaceDay;
 USE RaceDay;
+
+--create tables using the 7 entities from ERD
+
+--table users
+CREATE TABLE Users (
+    UserID          INT IDENTITY(1,1)   NOT NULL,
+    FullName        NVARCHAR(150)       NOT NULL,
+    Email           NVARCHAR(255)       NOT NULL,
+    PasswordHash    NVARCHAR(255)       NOT NULL,
+    Role            NVARCHAR(20)        NOT NULL DEFAULT 'Participant',
+    CreatedAt       DATETIME2           NOT NULL DEFAULT SYSDATETIME(),
+    CONSTRAINT PK_Users PRIMARY KEY (UserID),
+    CONSTRAINT UQ_Users_Email UNIQUE (Email),
+    CONSTRAINT CK_Users_Role CHECK (Role IN ('Organiser', 'Participant'))
+);
