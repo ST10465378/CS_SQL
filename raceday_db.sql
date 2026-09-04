74d7fe4c5e8bd16c02c79db6f19fb0bc1e175e0a
ST10465378 <ST10465378@rcconnect.edu.za>
2026-09-04 15:10:48 +0200

    create table Organisers

--- raceday_db.sql
+++ raceday_db.sql
@@ -16,3 +16,15 @@ CREATE TABLE Users (
     CONSTRAINT UQ_Users_Email UNIQUE (Email),
     CONSTRAINT CK_Users_Role CHECK (Role IN ('Organiser', 'Participant'))
 );
+
+--create table Organisers
+CREATE TABLE Organisers (
+    OrganiserID         INT IDENTITY(1,1)  NOT NULL,
+    UserID               INT                NOT NULL,
+    OrganisationName     NVARCHAR(150)      NOT NULL,
+    ContactPhone         NVARCHAR(30)       NULL,
+    CONSTRAINT PK_Organisers PRIMARY KEY (OrganiserID),
+    CONSTRAINT UQ_Organisers_UserID UNIQUE (UserID),
+    CONSTRAINT FK_Organisers_Users FOREIGN KEY (UserID)
+);
+
